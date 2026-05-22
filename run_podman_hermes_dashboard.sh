37eeca60e50ed4e57d9bc224eb767b66eb56011b
Aroen vR <aroenvr@macbookair.home>
2026-05-22 13:38:25 -0700

    docs: Improved the note left in the dashboard script

--- run_podman_hermes_dashboard.sh
+++ run_podman_hermes_dashboard.sh
@@ -8,4 +8,5 @@ podman run \
   hermes:v1 \
   hermes dashboard --no-open --host 127.0.0.1 --port 9119
 
-# Note: Open an SSH tunnel with `ssh -L 9119:127.0.0.1:9119 USER@SERVER_IP` to access the dashboard on your local machine.
+# Note: Open an SSH tunnel with `ssh -L 9119:127.0.0.1:9119 USER@SERVER_IP` 
+#       to access the dashboard on your local machine (http://127.0.0.1:9119/kanban)
