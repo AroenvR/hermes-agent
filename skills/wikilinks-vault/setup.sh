f8d38c420ae90dcc30713005aa3e9453bf603817
AroenvR <aroenvr@yolo.com>
2026-05-22 19:07:49 +0200

    todo: Added a stub setup file

--- skills/wikilinks-vault/setup.sh
+++ skills/wikilinks-vault/setup.sh
@@ -1,4 +1,4 @@
 # TODO:
-# - This script should create the vault directory
+# - This script should create the wikilinks' vault directory
 # - This script should create the empty /vault/INDEX.md file
 # - This script should create the empty /vault/TODO.md file
