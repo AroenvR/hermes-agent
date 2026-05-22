9b32644b631892ae28aadd309b764fcd4af85fed
AroenvR <aroenvr@yolo.com>
2026-05-22 22:06:07 +0200

    docs: Added a note to the dashboard script

--- run_podman_hermes_dashboard.sh
+++ run_podman_hermes_dashboard.sh
@@ -7,3 +7,5 @@ podman run \
   --network=host \
   hermes:v1 \
   hermes dashboard --no-open --host 127.0.0.1 --port 9119
+
+# Note: Open an SSH tunnel with `ssh -L 9119:127.0.0.1:9119 USER@SERVER_IP` to access the dashboard on your local machine.
