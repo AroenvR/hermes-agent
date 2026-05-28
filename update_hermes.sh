2d29e0671eddd9c59238449f208e05ed5cb35dfe
AroenvR <aroenvr@yolo.com>
2026-05-28 22:24:24 +0200

    update: Added no-cache to image building and added a version check

--- update_hermes.sh
+++ update_hermes.sh
@@ -1,3 +1,11 @@
 TAG=v0.15.0-24.04
 
-podman build --tag hermes:$TAG .
+podman build --no-cache --pull=always --tag hermes:$TAG .
+
+podman run --rm -it hermes:$TAG bash -lc '
+  whoami
+  node --version
+  python3 --version
+  command -v hermes
+  hermes --version || hermes version || true
+'
